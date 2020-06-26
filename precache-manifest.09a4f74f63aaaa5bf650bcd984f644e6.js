093766887e4177c87442d7b6ead8abaa6f53ef7a
Jaclyn Hardy <jackihardy@yahoo.com>
2020-06-25 20:21:20 -0700

    Updates

---
+++ precache-manifest.09a4f74f63aaaa5bf650bcd984f644e6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33ea38b537111cd7ed2f48c186ecef5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbdc7e71fa7224ca2529",
+    "url": "/static/js/2.67bf6da6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67bf6da6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0985b8dd5d412aac5d86",
+    "url": "/static/js/main.600e7b7b.chunk.js"
+  },
+  {
+    "revision": "e5c8f6e1d19224a70ee6",
+    "url": "/static/js/runtime-main.dd2e83ff.js"
+  }
+]);
